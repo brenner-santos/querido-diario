c9327c1677e2cf324a90d5391b99a4dcbe5c6001
Brenner <bs.brenner.santos@gmail.com>
2025-07-01 13:12:23 -0300

    Adiciona rs_barra_do_ribeiro

---
+++ data_collection/gazette/spiders/rs/rs_barra_do_ribeiro.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+from gazette.spiders.base.dioenet import BaseDioenetSpider
+
+
+class RsBentoGoncalvesSpider(BaseDioenetSpider):
+    TERRITORY_ID = "4301909"
+    name = "rs_barra_do_ribeiro"
+    BASE_URL = "https://plenussistemas.dioenet.com.br/list/barra-do-ribeiro"
+    power = "executive"
+    start_date = date(2022, 7, 28)
+    
